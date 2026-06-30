c63fceb765d9075bda534091a742599bc7972a67
Raj Shah <rajshah@MAC-KN37N94RXR.local>
2026-06-30 16:28:27 +0530

    Day 1 complete. connected TypeORM with postgresql

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,16 @@
-export class CreateUserDto {}
+import {IsEmail, IsString, MinLength} from 'class-validator';
+
+export class CreateUserDto {
+    @IsString()
+    firstName: string;
+
+    @IsString()
+    lastName: string;
+
+    @IsEmail()
+    email: string;
+
+    @IsString()
+    @MinLength(8)
+    password: string;
+}
