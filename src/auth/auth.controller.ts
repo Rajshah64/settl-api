719dfd1ab7f2d6cf8ecbc0f828611112570d9711
Raj Shah <rajviralshah@gmail.com>
2026-07-29 22:58:29 +0530

    Validation pipe and guards and jwt added.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, Body, HttpCode } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode, Get, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
 
 
 @Controller('auth')
@@ -15,8 +16,15 @@ export class AuthController {
   }
 
   @Post('login')
-  @HttpCode(200)
+  @HttpCode(201)
   login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
+  }
+
+  @Get('me')
+  @HttpCode(200)
+  @UseGuards(JwtAuthGuard)
+  me(@Request() req: any){
+    return req.user;
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,16 +3,34 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from '../users/user.module';
 import { JwtModule } from '@nestjs/jwt';
-import {StringValue} from 'ms';
+import {StringValue} from 'ms'
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { config } from 'process';
+import { JwtStrategy } from './jwt.strategy';
+
+console.log('JWT_SECRET:', process.env.JWT_SECRET);
+console.log('JWT_EXPIRY:', process.env.JWT_EXPIRY);
 
 @Module({
-  imports: [UserModule,JwtModule.register({
-    secret: process.env.JWT_SECRET,
-    signOptions: {
-      expiresIn: process.env.JWT_EXPIRY as StringValue,
-    },
-  }),],
+  
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.getOrThrow<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.getOrThrow<string>('JWT_EXPIRY') as StringValue,
+        } ,
+      }),
+    } 
+    ),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService,JwtStrategy],
 })
+
 export class AuthModule {}
